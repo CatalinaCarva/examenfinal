3fc1c1511229bcee05aa517474b2fd3f9e4f9718
CatalinaCarva <catalina.carvajal0907@uhispano.ac.cr>
2024-08-23 21:56:54 -0600

    Add files via upload

---
+++ catalinaexamenfinal/tablasCatalinaFinal.sql
@@ -0,0 +1,32 @@
+﻿
+CREATE DATABASE ControlViajeros;
+
+USE ControlViajeros;
+CREATE TABLE TiposDeMovimiento
+(
+    id INT PRIMARY KEY IDENTITY,
+    Descripcion VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Viajeros
+(
+    id INT PRIMARY KEY IDENTITY,
+    Nombre VARCHAR(50) NOT NULL,
+    Apellidos VARCHAR(50) NOT NULL,
+    TipoDocumento INT NOT NULL,
+    NumeroDocumento VARCHAR(30) UNIQUE NOT NULL,
+    Nacionalidad VARCHAR(50) NOT NULL,
+    FechaNacimiento DATE NOT NULL,
+    CONSTRAINT FK_TipoDocumento FOREIGN KEY (TipoDocumento) REFERENCES TiposDeMovimiento(id)
+);
+
+CREATE TABLE Movimientos
+(
+    id INT PRIMARY KEY IDENTITY,
+    Viajero INT NOT NULL,
+    FechaHora DATETIME NOT NULL,
+    TipoMovimiento INT NOT NULL,
+    Lugar VARCHAR(60) NOT NULL,
+    CONSTRAINT FK_Viajero FOREIGN KEY (Viajero) REFERENCES Viajeros(id),
+    CONSTRAINT FK_TipoMovimiento FOREIGN KEY (TipoMovimiento) REFERENCES TiposDeMovimiento(id)
+);
